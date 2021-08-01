2c348606ba5c7b378a5a53932595e115645d4ed4
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-01 00:52:37 -0300

    Personalização de pontos de interrupção

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {},
+    screens: {
+      sm: '600px',
+      md: '960px',
+      lg: '1280px',
+      xl: '1920px',
+      print: { raw: 'print' }
+    }
   },
   variants: {
     extend: {}
